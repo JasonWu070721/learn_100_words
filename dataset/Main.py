e6d8d463c48fc0532be6d99a4d635cfc466728d7
Jason Wu <jason_wu@aai.com.tw>
2022-10-06 22:47:06 +0800

    Create words database file

--- dataset/Main.py
+++ dataset/Main.py
@@ -2,6 +2,54 @@ import pandas as pd
 import numpy as np
 import re
 import json
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        # print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def sql_execute(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+
+def sql_executemany(conn, create_table_sql, data):
+    try:
+        c = conn.cursor()
+        c.executemany(create_table_sql, data)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+
+def create_sql_table(conn):
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS words (
+                                        id integer PRIMARY KEY,
+                                        en text,
+                                        zh_tw text,
+                                        part_of_speech text,
+                                        type text,
+                                        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+                                    ); """
+    if conn is not None:
+        sql_execute(conn, sql_create_projects_table)
+    else:
+        print("Error! cannot create the database connection.")
 
 
 def words_to_array(detail):
@@ -36,6 +84,18 @@ if __name__ == '__main__':
     xls_page_list = ['1級', '2級', '3級', '4級', '5級']
     level_list = ['level_1', 'level_2', 'level_3', 'level_4', 'level_5']
 
+    conn = create_connection("database.db")
+
+    create_sql_table(conn)
+
+    lang_list = [
+        ("wharf", "碼頭;停泊處", "level_1"),
+        ("wildlife", "野生生物", "level_1"),
+        ("vow", "誓言;誓約", "level_1"),
+    ]
+    sql_executemany(
+        conn, "insert into words(en, zh_tw, type) values (?, ?)", lang_list)
+
     xls = pd.ExcelFile('senior_7000.xls')
 
     for idx,  level in enumerate(xls_page_list):
@@ -52,4 +112,3 @@ if __name__ == '__main__':
 
         json_file_name = level_list[idx] + '.json'
         write_json_file(level_arr, json_file_name)
-        print(level_arr)
